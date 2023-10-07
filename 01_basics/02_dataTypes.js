005ba2ead2325d8330dc3988331e6405cbacd9df
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-07 14:48:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+1)  // we are using node js not browser
+
+let age = 23;
+ 
+let isLoggedIn = true;
+
+let state = null;
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> stand alone value
+// symbol ==> unique
+
+
+// Object
+
+console.log(typeof undefined);
+
+console.log(typeof null);
