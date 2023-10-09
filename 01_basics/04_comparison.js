1fe81047b46aa4b0e41708732aa0bf8bb154ca27
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-09 06:53:39 +0000

    go through with comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("2" > 1);
+
+// always remember that the equality check (==) and comparison opertor ("<,>") always work differently
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 1);
+// console.log(undefined < 1);
+// console.log(undefined >= 1);
+// console.log(undefined == 1);
+
+// === strict equalty check
+ // console.log("2" === 2); //("===") it also checks datatype 
