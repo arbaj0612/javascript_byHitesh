2608676ff9a2d154cd6516d4aaf7cb781fb19cdc
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-09 03:04:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,66 @@
+let score  = 35
+
+console.log(typeof score); //datatype number
+console.log(typeof (score));  
+
+let value = Number(score)
+console.log(value);
+
+let badScore = "35ab"
+console.log(typeof badScore); // datatype string
+
+let converInNum = Number(badScore)
+console.log(converInNum);     // output = NaN
+console.log(typeof converInNum); // datatype number
+
+
+let newScore = "35"
+console.log(typeof newScore); // datatype string
+
+let valueInNumber = Number(newScore)
+console.log(typeof valueInNumber); // datatype number
+
+let myScore = null
+console.log(typeof myScore); // datatype object
+
+let getScore = Number(myScore)
+console.log(getScore); // output= 0
+console.log(typeof getScore); // datatype number
+
+let yourScore = undefined
+console.log(typeof yourScore); // datatype undefined
+
+let getYourScore = Number(yourScore)
+console.log(getYourScore);  // NaN i.e not a number
+console.log(typeof getYourScore); // datatype number
+
+
+let finalScore = true
+console.log(typeof finalScore); // datatype boolean
+
+let getFinalScore = Number(finalScore)
+console.log(getFinalScore); // output = 1
+console.log(typeof getFinalScore); // datatype number
+
+
+// "35" ==> 35
+// "35ab" ==> NaN
+// true ==> 1 ; false ==> 0
+
+
+let isLoggedIn = "arbaj"
+
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged); // true
+console.log(typeof booleanIsLogged); //boolean 
+
+// 1 ==> true ; 0 ==> false
+// "" ==> false
+// "arbaj" ==> true
+
+
+let someNumber = 35
+let convertInString = String(someNumber)
+
+console.log(convertInString); // 35
+console.log(typeof convertInString);  // string
