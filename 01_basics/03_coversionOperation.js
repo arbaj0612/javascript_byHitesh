f108fe8cb5db378d3034ffa148355c39a6d0309f
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-09 03:36:42 +0000

    more conversion of datatypes

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -1,46 +1,46 @@
 let score  = 35
 
-console.log(typeof score); //datatype number
-console.log(typeof (score));  
+  // console.log(typeof score); //datatype number
+  // console.log(typeof (score));  
 
 let value = Number(score)
-console.log(value);
+  // console.log(value);
 
 let badScore = "35ab"
-console.log(typeof badScore); // datatype string
+ // console.log(typeof badScore); // datatype string
 
 let converInNum = Number(badScore)
-console.log(converInNum);     // output = NaN
-console.log(typeof converInNum); // datatype number
+  // console.log(converInNum);     // output = NaN
+  // console.log(typeof converInNum); // datatype number
 
 
 let newScore = "35"
-console.log(typeof newScore); // datatype string
+ // console.log(typeof newScore); // datatype string
 
 let valueInNumber = Number(newScore)
-console.log(typeof valueInNumber); // datatype number
+ // console.log(typeof valueInNumber); // datatype number
 
 let myScore = null
-console.log(typeof myScore); // datatype object
+ // console.log(typeof myScore); // datatype object
 
 let getScore = Number(myScore)
-console.log(getScore); // output= 0
-console.log(typeof getScore); // datatype number
+ // console.log(getScore); // output= 0
+ // console.log(typeof getScore); // datatype number
 
 let yourScore = undefined
-console.log(typeof yourScore); // datatype undefined
+ // console.log(typeof yourScore); // datatype undefined
 
 let getYourScore = Number(yourScore)
-console.log(getYourScore);  // NaN i.e not a number
-console.log(typeof getYourScore); // datatype number
+ // console.log(getYourScore);  // NaN i.e not a number
+ // console.log(typeof getYourScore); // datatype number
 
 
 let finalScore = true
-console.log(typeof finalScore); // datatype boolean
+ // console.log(typeof finalScore); // datatype boolean
 
 let getFinalScore = Number(finalScore)
-console.log(getFinalScore); // output = 1
-console.log(typeof getFinalScore); // datatype number
+ // console.log(getFinalScore); // output = 1
+ // console.log(typeof getFinalScore); // datatype number
 
 
 // "35" ==> 35
@@ -51,8 +51,8 @@ console.log(typeof getFinalScore); // datatype number
 let isLoggedIn = "arbaj"
 
 let booleanIsLogged = Boolean(isLoggedIn)
-console.log(booleanIsLogged); // true
-console.log(typeof booleanIsLogged); //boolean 
+ // console.log(booleanIsLogged); // true
+ // console.log(typeof booleanIsLogged); //boolean 
 
 // 1 ==> true ; 0 ==> false
 // "" ==> false
@@ -62,5 +62,44 @@ console.log(typeof booleanIsLogged); //boolean
 let someNumber = 35
 let convertInString = String(someNumber)
 
-console.log(convertInString); // 35
-console.log(typeof convertInString);  // string
+ // console.log(convertInString); // 35
+ // console.log(typeof convertInString);  // string
+
+
+
+ // ************************** OPERATIONS *********************************
+
+let newvalue = 4 
+let negNewValue = -newvalue
+ // console.log(negNewValue);
+
+   
+  // console.log(2+2);
+  // console.log(2-2);
+  // console.log(2*2);
+  // console.log(2**3);
+  // console.log(2/2);
+  // console.log(2%3);
+
+
+  let str = "Hello" 
+  let str1 = " Arbaj"
+  let str2 = str + str1
+   // console.log(str2);
+
+ // console.log("1" + 2);
+ // console.log("1" + "2");
+ // console.log("1" +  +"2");
+ // console.log(1 + 2 + "2");
+ // console.log("1" + 2 + 2);
+ // console.log(+"1" + 2 + 2); 
+
+  // console.log(+true); 
+  // console.log(+false);
+  // console.log(+"");
+
+
+  let gameCounter = 100
+  let newCounter = gameCounter++
+   // console.log(gameCounter);
+   // console.log(newCounter);
