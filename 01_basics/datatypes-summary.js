0c274bba74fdbbd661a335f2ecbd85a9413bbe5f
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-10 03:13:39 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,15 +10,15 @@ let isPlaying = true
 let player;
 let newscore = null
 
-console.log(typeof newscore); // typeof null is object
+// console.log(typeof newscore); // typeof null is object
 
 let myscore = Symbol(60)
 let yourScore = Symbol(60)
 
-console.log(myscore === yourScore);
+// console.log(myscore === yourScore);
 
 const bigNumber  = 273487236793298865n
-console.log( typeof bigNumber); // bigint
+// console.log( typeof bigNumber); // bigint
 
 
 // 2 ==> Reference or Non-primitive datatypes
@@ -26,7 +26,7 @@ console.log( typeof bigNumber); // bigint
 // Datatypes ==> Array , Object , functions
 
 const fruits = ["mango" , "apple", "banana", "qivi"]
-console.log(typeof fruits); // object
+// console.log(typeof fruits); // object
 
 
 
@@ -37,13 +37,47 @@ const myObj = {
     isWorking: true
 }
 
-console.log(typeof myObj); // object
+// console.log(typeof myObj); // object
 
 const myfunction = function (){
-    console.log("Hello World");
+    // console.log("Hello World");
 }
 
 myfunction()
 
-console.log(typeof myfunction); // function or (Object function)
+// console.log(typeof myfunction); // function or (Object function)
 
+
+
+
+//******************************** Stack and Heap memory *********************************
+
+// Stack (Primitive values)
+let myAge = 22
+
+let yourAge = myAge
+yourAge = 24
+
+console.log(myAge);
+console.log(yourAge); 
+
+// In stack memory allocation we get copy of the variables so here when we changed the value of yourAge it does not change the 
+// value of variable myAge
+
+
+// Heap (Non - Primitive)
+
+let user1 = {
+    email: "aj009@gmail.com",
+    upi: "1234@ybl"
+}
+
+let user2 = user1
+
+user2.email = "am@yahoo.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+// In heap memory allocation we get the reference of the values so whenever we change value of duplicates it will also affect the 
+// original ones 
