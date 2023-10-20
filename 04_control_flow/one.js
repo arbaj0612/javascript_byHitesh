b6653ffbe7c0b069d9dd90e7894155769b019a1e
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-20 14:47:33 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if-else
+
+// const temperature = 55
+
+// if (temperature < 50) {
+//     console.log("Temperature is less than 50");
+// } else {
+//     console.log("Temperature is greater than 50");
+// }
+
+// const score  = 200 
+// if (score > 100) {
+//     let power = "Fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);  erroe ==> power not defined bcoz we have used let keyword and ie blocked scope
+
+
+const balance  = 500 
+ if (balance > 500) {
+    console.log("greater than 500");
+ } else if (balance < 400) {
+    console.log("low balance");
+ } else if (balance >= 500) {
+    console.log("correct balance");
+ }else{
+    console.log("zero balance");
+ }
