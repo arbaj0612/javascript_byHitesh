f30751e1f39a3f99524bfc777520fb9dc89c98a8
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-21 15:01:14 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,55 @@
+// for
+ 
+for (let i = 0; i < 10 ; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+
+for (let i = 1; i <= 10; i++){
+    // console.log(`Outer loop value: ${i}`);
+    for(let j = 1; j <= 10; j++){
+        //  console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + "*" + j + " = " + i*j);
+
+    }
+}
+
+
+//table of 5
+for (let i = 1; i <= 10; i++) {
+    let number = 5
+//    console.log(number * i);
+    
+}
+
+// array itertion
+const myArray = ["arbaj" , "Ayesha", "Parveen"]
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    // console.log(element);
+    
+}
+
+// break and continue
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 7) {
+//         console.log("7 Detected");
+//         break;
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 7) {
+//         console.log("7 Detected");
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
