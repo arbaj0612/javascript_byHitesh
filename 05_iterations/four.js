ff55680881802e7a2e7fe39e1b17be7930266094
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-23 04:18:10 +0000

    for-in/for-off loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,20 @@
+// for in loop
+
+// const myObj = {
+//     js : "javascript",
+//     cpp : "c++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// }
+
+// for (const key in myObj) {
+//   console.log(`${key} is shortcut for ${myObj[key]}`);
+// }
+
+// const myArr = ["ab", "bc", "xy", "pq"]
+
+// for (const key in myArr) {
+//     // console.log(key); // we get only index number using for in loop
+//     console.log(myArr[key]);  // will get values
+// }
+
---
+++ 05_iterations/three.js
@@ -0,0 +1,45 @@
+// for of
+
+// const myArray = [11, 22, 33, 44, 55, 66]
+
+// for (const nums of myArray) {
+//     console.log(`here they are ${nums}`); // we get direct values of array usig for of loop
+// }
+
+// const greetings = "Hello World!"
+
+// for (const greet of greetings) {
+//     if(greet == " "){
+//         continue
+//     }
+//     console.log(`Each char: ${greet}`);
+// }
+
+
+
+// Maps ==> it always provide new values and neglect duplicates
+
+// const map = new Map()
+
+// // to set values
+
+// map.set("In" , "India")
+// map.set("UAE" , "United Arab Emirates")
+// map.set("PL" , "Palestine")
+
+// console.log(map); // will get key value pair in object as output
+
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value);
+// }
+
+
+
+// const myObj = {
+//     name: "arbaj",
+//     name2: "ayesha"
+// }
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ":-" , value);  // object can be iterable like this
+// }
