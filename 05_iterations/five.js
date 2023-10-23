eac1d0eb3ae4dcd6f6d927090dc8e7022e47aac7
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-23 14:07:11 +0000

    inbuilt iteration methods

---
+++ 05_iterations/five.js
@@ -0,0 +1,50 @@
+//forEach
+
+const coding = ["js", "html" , "cpp", "ruby", "java"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// arrow function
+// coding.forEach( (val)=> {
+//     console.log(val);
+// })
+
+// function getValues (item) {
+//     console.log(item);
+// }
+
+// coding.forEach(getValues);  // giving reference of above function
+
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// })
+
+// coding.map( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// })
+
+
+// array of objects
+// [{},{},{}]
+
+const myCoding = [
+    {   language: "javascript",
+        fileName: "js"
+    }, 
+    {
+       language: "Python",
+       fileName: "Py" 
+    },
+    {
+        language:"java",
+        fileName: "javax"
+    }
+]
+
+
+// myCoding.forEach( (item)=> {
+//     console.log(item.language);
+// })
