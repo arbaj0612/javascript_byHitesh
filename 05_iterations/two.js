89f22fb533df264b19ea2f98472918eb580d45c9
Arbaj Mulani <125712614+arbaj0612@users.noreply.github.com>
2023-10-23 03:29:16 +0000

    do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while loop 
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let myArray = ["apple", "mango", "dates"]
+// let i = 0
+// while (i < myArray.length) {
+//     console.log(`Values are are ${myArray[i]}`);
+//     i++
+// }
+
+
+// do-while 
+
+// let score = 0
+
+// do {
+//     console.log(`play for score ${score}`);
+//     score ++
+
+// } while (score <= 10)
