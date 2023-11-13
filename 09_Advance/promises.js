83f46ae3ada654bebe27a8fbb7cab2b5e9741e1f
arbaj0612 <junaid.786mulani@gmail.com>
2023-11-13 19:15:41 +0530

    deep dive in promises

--- 09_Advance/promises.js
+++ 09_Advance/promises.js
@@ -57,9 +57,10 @@ promiseThree.then( (user) => {
 
 const promiseFour = new Promise (function (resolve , reject) {
     setTimeout(function () {
+        console.log("Async task 5 done");
         let error = true
         if (!error) {
-            resolve({username: "Arbaj" , pass: "1234"})
+            resolve({username: "Arbaj" , password: "1234"})
         } else {
             reject("ERROR : Something went wrong")
         }
@@ -69,11 +70,58 @@ const promiseFour = new Promise (function (resolve , reject) {
 
 promiseFour.then( (user) => {
     console.log(user);
-    return user.username
-}).then( (getUserName) => {
-    console.log(getUserName);
+    return user.password
+}).then( (getPass) => {  // we have done the chaining here (accessing the above return value)
+    console.log(getPass);
 }).catch( (error) => {
     console.log(error);
 }).finally( () => console.log("Promise is either resolved or rejected"));
 
 
+
+const promiseFive = new Promise (function (resolve , reject) {
+    setTimeout(function () {
+        console.log("Async task 6 done");
+        let error = true
+        if (!error) {
+            resolve({username: "Javascript" , password: "Js1234"})
+        } else {
+            reject("ERROR : js went wrong")
+        }
+    },5000)
+})
+
+// NOTE : async/await cant handle the errors directly so we have to wrap them in try and catch
+
+
+async function consumPromise() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+    console.log(error);
+    }
+}
+
+consumPromise()
+
+
+async function getAllUsers () {
+    const response = await fetch ("https://jsonplaceholder.typicode.com/users")
+    // console.log(response)
+   const data =   await response.json()
+   console.log(data);
+}
+
+// getAllUsers()
+
+
+
+fetch("https://api.github.com/users/arbaj0612")
+.then( (response) => {
+    return response.json()
+})
+.then( (data) => {
+    console.log(data);
+})
+.catch( (error) => console.log("E :" , "got error"))
